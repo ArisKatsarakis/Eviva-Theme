d08449f6cc5968e632e0a574578ec0f246c305f9
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-04-04 23:46:23 +0300

    try 2

--- header-home.php
+++ header-home.php
@@ -7,7 +7,7 @@
   ?>
 
   <?php
-  if (function_exists('the_custom_logo')) {
+  if (isset(function_exists('the_custom_logo'))) {
     /* the_custom_logo(); */
     $custom_logo_id = get_theme_mod('custom_logo');
     $logo = wp_get_attachment_image_src($custom_logo_id)[0];
