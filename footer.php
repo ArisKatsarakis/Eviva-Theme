bec01bfc37b942996686c4966255a9353d12d995
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-04-04 23:29:07 +0300

    adding footer nulling collision

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 wp_footer();
-if (function_exists('the_custom_logo')) {
+if (function_exists('the_custom_logo') ??  true) {
   /* the_custom_logo(); */
   $custom_logo_id = get_theme_mod('custom_logo');
   $logo = wp_get_attachment_image_src($custom_logo_id)[0];
