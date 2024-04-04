11abe22d5ad361d9ed0c1b6d1b14ca69dcebceaa
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-04-04 23:47:51 +0300

    test 3

--- header-home.php
+++ header-home.php
@@ -7,10 +7,9 @@
   ?>
 
   <?php
-  if (isset(function_exists('the_custom_logo'))) {
+  if (function_exists('the_custom_logo')) {
     /* the_custom_logo(); */
     $custom_logo_id = get_theme_mod('custom_logo');
-    $logo = wp_get_attachment_image_src($custom_logo_id)[0];
   }
   ?>
   <link rel="preconnect" href="https://fonts.googleapis.com">
